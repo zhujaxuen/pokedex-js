3b117029ed227601b92995aa2fd92f8058e756bf
Andrew Zhu <andrewdzhu@gmail.com>
2026-01-17 21:12:56 -0300

    Initial commit - Pokedex em JavaScript

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemong__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.button--prev');
+const buttonNext = document.querySelector('.button--next');
+
+let searchPokemon = 1;
+
+const fecthPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (APIResponse.status === 200) { 
+        const data = await APIResponse.json();
+        return data; 
+    }
+};
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fecthPokemon(pokemon);
+    console.log(data);
+    
+    if (data) {
+        pokemonImage.style.display = 'block';        
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonNumber.innerHTML = '';
+        searchPokemon = 1;
+    }
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    if ( searchPokemon > 1 ) {            
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);    
+});
+
+renderPokemon(searchPokemon);
